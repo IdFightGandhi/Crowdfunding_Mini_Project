b4c717ba153777b9af82669a5f990261516a6783
Jennifer Vereecken <77600929+jbeans12@users.noreply.github.com>
2021-03-24 14:23:05 -0700

    more stuff

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const loginRoutes = require('./login-routes');
 
 router.use('/api', apiRoutes);
 
 router.use('/', homeRoutes);
+
+router.use('/login', loginRoutes);
 
 module.exports = router;
---
+++ Develop/controllers/login-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { User } = require('../models');
+
+router.post('/login', async (req, res) => {
+    try {
+      const loginData = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+  
+      if (!loginData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+  
+      const validPassword = await loginData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password. Please try again!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res
+          .status(200)
+          .json({ user: loginData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+  // Logout
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
+  
